3b7e744c3c2f8de0d2c3f2064ccc004dfea1ed85
pedrocarv <pedrohcptrabalho@gmail.com>
2025-09-30 19:53:05 -0300

    Update Drone.java
    Versão 2 do projeto - Pequena atualização nos  dados

--- Drone.java
+++ Drone.java
@@ -1,37 +1,37 @@
 /*
- Implementação do cadastro de drones 
+ Implementação da classe de drones 
  -> ID
  -> Status da bateria
  -> Capacidade de carga
- -> Disponibilidade (está em entrega ou livre) *
-
-Dados de cadastro acrescentados em relação ao mínimo proposto estão marcados com '*'
+ -> Disponibilidade (está em entrega ou livre) 
  */
-
 public class Drone {
     private int id;
-    private int statusBateria;
-    private double capacidadeCarga;
+    private int statusBateria; // 0 a 100
+    private int capacidade;    // quantidade máxima que pode carregar
     private boolean disponivel;
 
-    // Construtor:
-    public Drone(int statusBateria, double capacidadeCarga, boolean disponivel) {
+    // Construtor     
+    public Drone(int id, int statusBateria, int capacidade, boolean disponivel) {
+        if (statusBateria < 0 || statusBateria > 100) {
+            throw new IllegalArgumentException("Status da bateria deve ser entre 0 e 100.");
+        }
+        this.id = id;
         this.statusBateria = statusBateria;
-        this.capacidadeCarga = capacidadeCarga;
+        this.capacidade = capacidade;
         this.disponivel = disponivel;
     }
-    // Getters e setters:
+
+    // Getters e Setters
     public int getId() { return id; }
 
-    public void setId(int id) { this.id = id; }
-
     public int getStatusBateria() { return statusBateria; }
-
-    public double getCapacidadeCarga() { return capacidadeCarga; }
-
+    
+    public void setStatusBateria(int bateria){ this.statusBateria = bateria;}
+    
+    public int getCapacidade() { return capacidade; }
+    
     public boolean isDisponivel() { return disponivel; }
-
-    public void setDisponivel(boolean disponivel) {
-        this.disponivel = disponivel;
-    }
+    
+    public void setDisponivel(boolean disponivel) { this.disponivel = disponivel; }
 }
